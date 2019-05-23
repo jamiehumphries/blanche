e7fab4c237e2406ab545bc34a439f47f474ace04
Jamie Humphries <j@miehumphri.es>
2019-05-23 21:38:46 +0100

    Update list of available Pokémon.

--- unavailable.js
+++ unavailable.js
@@ -6,28 +6,8 @@ module.exports = new Set([
   352, // Kecleon
   385, // Jirachi
   // SINNOH
-  412, // Burmy
-  413, // Wormadam
-  414, // Mothim
-  420, // Cherubi
-  421, // Cherrim
-  422, // Shellos
-  423, // Gastrodon
   439, // Mime Jr.
-  443, // Gible
-  444, // Gabite
-  445, // Garchomp
-  449, // Hippopotas
-  450, // Hippowdon
-  462, // Magnezone
-  470, // Leafeon
-  471, // Glaceon
-  476, // Probopass
   479, // Rotom
-  480, // Uxie
-  481, // Mesprit
-  482, // Azelf
-  483, // Dialga
   486, // Regigigas
   489, // Phione
   490, // Manaphy
